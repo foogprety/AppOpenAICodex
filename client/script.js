5a5a72b727e01e988cf7e4c22e8db4d5bf534f3c
Travail <Travail@DJIB-CONSULTANT>
2023-01-25 23:04:50 -0500

    commit

--- client/script.js
+++ client/script.js
@@ -93,7 +93,7 @@ const handleSubmit = async(e) =>{
   //fetsh data from server
 
 console.log( '**************' + data.get('prompt'));
-  const response = await fetch('http://localhost:5000',{
+    const response = await fetch('https://codex-k9c6.onrender.com',{
     method: 'POST',
     headers : {
       'Content-Type' : 'application/json'
